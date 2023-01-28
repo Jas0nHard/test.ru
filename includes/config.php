4f64ed2e96ee08da605977c2271c6a97c1198899
Jas0nHard <jasyanovsky@gmail.com>
2023-01-28 18:51:44 +0300

    onlyworkingphp

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array(
+	'title' => 'Блог IT_Минималиста',
+	'vk_url' => 'http://vk.com/somebody',
+
+	'db' => array(
+			'server' => 'localhost',
+			'username' => 'root',
+			'password' => '',
+			'name' => 'test_blog'
+		)
+);
+
+require "db.php";
